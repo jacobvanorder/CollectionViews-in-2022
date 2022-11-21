cbba0a5448b9705eb308c386a9bd56b12f7cb10d
Jacob Van Order <jacob@sushigrass.com>
2022-11-21 10:32:20 -0600

    Empty project.

---
+++ CollectionViews In 2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViews In 2022
+//
+//  Created by Jacob Van Order on 11/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CollectionViews In 2022UITests/CollectionViews_In_2022UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CollectionViews_In_2022UITestsLaunchTests.swift
+//  CollectionViews In 2022UITests
+//
+//  Created by Jacob Van Order on 11/21/22.
+//
+
+import XCTest
+
+final class CollectionViews_In_2022UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
